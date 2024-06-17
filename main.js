62d39871852c44a9a7771bb068072d737434a657
Daria Ilina <darlail.work.mail@gmail.com>
2024-06-17 21:18:38 +0300

    Main page files

---
+++ main.js
@@ -0,0 +1,4 @@
+gsap.to(".container", { opacity: 1, duration: 1, ease:'power1.in', delay: 0.5})
+gsap.to("a", { opacity: 1, duration: 1, stagger: 0.5, ease:'power1.in', delay: 0.7})
+gsap.to("hr", { opacity: 1, duration: 1, stagger: 0.5, ease:'power1.in', delay: 0.8})
+gsap.to("h1", { opacity: 1, duration: 1 , ease:'power1.in', delay: 0.55})
